af455247adc3bf0aae82f9fdcb0b1b3639b7a32c
romanbatavi <romanbatavi98@gmail.com>
2021-05-27 19:21:35 +0700

    syling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import BgImg from '../assets/bg-image.png'
+import BgImg from '../assets/bg-image.png';
 
 const Section = styled.section`
   background: url(${BgImg});
@@ -14,10 +14,42 @@ const Content = styled.div`
   height: 100px;
 `;
 
-const Left = styled.div``
-const Title = styled.p``
-const Desc = styled.p``
-const Button = styled.a``
+const Left = styled.div`
+  padding-left: 220px;
+  padding-top: 143px;
+`;
+
+const Title = styled.p`
+  font-size: 55px;
+  color: #04050a;
+  font-weight: 400;
+`;
+
+const Desc = styled.p`
+  width: 472px;
+  font-size: 20px;
+  color: #9ea0ac;
+  line-height: 30px;
+  margin-top: 58px;
+`;
+
+const Button = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 18px;
+  margin-top: 58px;
+  width: 371px;
+  height: 71px;
+  line-height: 71px;
+  font-size: 22px;
+  text-align: center;
+  color: #fff;
+  cursor: pointer;
+  background: linear-gradient(98deg, #0546d6, #3f89fc);
+  text-decoration: none;
+  box-shadow: 0 15px 14px rgb(0 42 177 / 12%);
+`;
 
 const Hero = () => {
   return (
