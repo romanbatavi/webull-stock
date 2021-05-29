34f41e5bdebb8df052d945f4e1b1e1229dd9be08
romanbatavi <romanbatavi98@gmail.com>
2021-05-29 22:37:04 +0700

    finish styling navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,12 +35,61 @@ const Nav = styled.nav `
   padding-left: 50px;
 `;
 
-const NavLink = styled.(Link) ``;
-const Search = styled.div ``;
-const SearchWrap = styled.div ``;
-const Input = styled.input ``;
-const ButtonContainer = styled.div ``;
-const Button = styled.div ``;
+const NavLink = styled(Link)`
+  color: #000;
+  padding: 0 15px;
+  font-size: 16px;
+  line-height: 80px;
+  font-weight: 700;
+  text-decoration: none;
+`;
+
+const Search = styled.div `
+  right: 235px;
+  position: absolute;
+`;
+const SearchWrap = styled.div `
+  width: 220px;
+  height: 36px;
+  position: relative;
+  box-sizing: border-box;
+  background: rgba(179, 191, 201, 0.15);
+  line-height: 33px;
+  padding-left: 37px;
+  border-radius: 19px;
+`;
+
+const Input = styled.input `
+  color: #000;
+  width: 196px;
+  border: transparent;
+  font-size: 12px;
+  background: transparent;
+  outline: none;
+
+  &::placeholder {
+    color: #d1d8de;
+  }
+`;
+
+const ButtonContainer = styled.div `
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Button = styled.div `
+  width: 100px;
+  color: #fff;
+  cursor: pointer;
+  height: 36px;
+  font-size: 16px;
+  box-sizing: border-box;
+  background: #007cff;
+  text-align: center;
+  line-height: 36px;
+  border-radius: 21px;
+`;
 
 const Navbar = () => {
   return (
